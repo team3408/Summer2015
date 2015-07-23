f4f8fdcb3decd1302d0dfbad6877aedbd17df402
Bridget <bridgetj18@users.noreply.github.com>
2015-07-23 15:37:00 -0700

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,37 @@
+
+#include "Arduino.h"
+#include "KCswerve310.h"
+
+#include <SPI.h>
+#include <SD.h>
+#include <Ethernet.h>
+#include <Servo.h>
+#include <EEPROM.h>
+#include <ROEncoder.h>
+#include <algorithm>
+using namespace std;
+
+vector::vector (double x, double y){
+        xx = x;
+        yy = y;
+        magnitude = sqrt( xx * xx + yy * yy ); // uses standard library
+        rotation = acos( xx / magnitude );
+        rotation = yy > 0.0 ? rotation : 2.0*PI - rotation;//acos is imperfect
+}
+
+void vector::rotate(double radians) {
+        rotation += radians;
+        while (rotation >= 2.0 * PI){rotation -= 2.0*PI;}// only + values
+        while (rotation < 0) {rotation += 2.0 * PI;}
+        xx = magnitude * cos(rotation);
+        yy = magnitude * sin(rotation);
+} 
+
+vector* vector::operator + (vector& second){
+        return new vector (xx + second.x(),yy + second.y() );
+}
+
+double vector::x(){ return xx; }
+double vector::y(){ return yy; }
+double vector::m(){ return magnitude; }
+double vector::r(){ return rotation; }
